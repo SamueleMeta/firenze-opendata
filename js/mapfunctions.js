4fb95c3e23efd9ea6263475ced5add9e17eb71db
Samuele Meta <metasamuele@gmail.com>
2018-07-01 23:28:17 +0200

    Added search to options menu

--- js/mapfunctions.js
+++ js/mapfunctions.js
@@ -1,20 +1,34 @@
 // JQUERY FUNCTIONS
 $('#pac-input').on("focus", function () {
-    $('#searchIcon').attr("src", "img/green-searcher.png");
+    $('.searchIcon').attr("src", "img/green-searcher.png");
 });
 
 $('#pac-input').on("blur", function () {
-    $('#searchIcon').attr("src", "img/searcher.png");
+    $('.searchIcon').attr("src", "img/searcher.png");
+});
+
+$('#pac-input-options').on("focus", function () {
+    $('.searchIcon').attr("src", "img/green-searcher.png");
+});
+
+$('#pac-input-options').on("blur", function () {
+    $('.searchIcon').attr("src", "img/searcher.png");
 });
 
 $('.service-info').on("click", function () {
     displayMarkers(this.parentNode.parentNode.id);
     $('#pac-input').hide();
     document.getElementById("sideOptions").classList.toggle('active');
+    setTimeout(function () {
+        $(".pac-container").prependTo("#searchResultsOptions");
+    }, 300);
 });
 
 $('#cancelIcon').on("click", function () {
     document.getElementById("sideOptions").classList.toggle('active');
+    setTimeout(function () {
+        $(".pac-container").prependTo("#searchResults");
+    }, 300);
     $('#pac-input').delay(290).show(0);
 });
 
@@ -208,15 +222,67 @@ function initAutocomplete() {
         $(".pac-container").prependTo("#searchResults");
     }, 300);
 
+    var inputOptions = document.getElementById('pac-input-options');
+    var searchBoxOptions = new google.maps.places.SearchBox(inputOptions);
+
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function () {
         searchBox.setBounds(map.getBounds());
+    });
+
+    map.addListener('bounds_changed', function () {
+        searchBoxOptions.setBounds(map.getBounds());
     });
 
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     searchBox.addListener('places_changed', function () {
         var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+
+    searchBoxOptions.addListener('places_changed', function () {
+        var places = searchBoxOptions.getPlaces();
 
         if (places.length == 0) {
             return;
